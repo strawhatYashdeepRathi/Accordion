0c4eb4333d2427ba6c1357d9e65a2b410bae9e81
Yashdeep Rathi <yashdeeprathi648@gmail.com>
2022-01-18 01:53:21 +0530

    allopenaccordion

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './style.css';
+import Accordion from './Accordion';
+import { Data } from './utils/data';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>All Open Accordion</h1>
+      <div className='Accordion'>
+        {Data.map(({ title, content }) => (
+            <Accordion title={title} content={content} />
+          ))}
+      </div>
     </div>
   );
 }
