28070d0905fa7b68a316e226fb663b8a9761751e
Yashdeep Rathi <yashdeeprathi648@gmail.com>
2022-01-18 17:08:11 +0530

    MulOpenAcc

--- my-app/src/Accordion.js
+++ my-app/src/Accordion.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import './style.css'
 
 
 const Accordion = ({ title, content }) => {
-  //  const [isActive, setIsActive] = useState(false);
+    const [isActive, setIsActive] = useState(false);
   return (
-    <div className="accordion-item">
-    <div className="accordion-title">
+    <div className='acc'>
+  <div className="accordion-item">
+    <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
       <div>{title}</div>
-      <div>{'-'}</div>
+      <div className='expansion'>{isActive ? '-' : '+'}</div>
     </div>
-    <div className="accordion-content">{content}</div>
+    {isActive && <div className="accordion-content">{content}</div>}
   </div>
+</div>
   );
 };
 export default Accordion;
