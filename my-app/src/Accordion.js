763214a0ef6d7a16cafcd1232aaab025ade25095
Yashdeep Rathi <yashdeeprathi648@gmail.com>
2022-01-18 18:14:56 +0530

    onlyOne

--- my-app/src/Accordion.js
+++ my-app/src/Accordion.js
@@ -1,19 +1,40 @@
 import React, { useState }  from 'react';
 import './style.css'
+import { Data } from './utils/data';
 
 
-const Accordion = ({ title, content }) => {
+const Accordion = () => {
     const [isActive, setIsActive] = useState(false);
+
+    const toggle = index =>{
+      if(isActive === index){
+        return setIsActive(null);
+    }
+    setIsActive(index);
+    }
   return (
-    <div className='acc'>
-  <div className="accordion-item">
-    <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
-      <div>{title}</div>
-      <div className='expansion'>{isActive ? '-' : '+'}</div>
+    <div>
+      <h1>OnlyOne Open Accordion</h1>
+      <div>
+        <div>
+          {Data.map((item, index) => {
+              return (
+                <div className='acc'>
+                  <div className='title' onClick={() => toggle(index)} key={index}>
+                    <h3>{item.title}</h3>
+                    <span className='plus'>{isActive === index ? '➖' : '➕'}</span>
+                  </div>
+                  {isActive === index ? (
+                    <div className='content'>
+                      <p>{item.content}</p>
+                    </div>
+                  ) : null}
+                </div>
+              );
+            })}
+        </div>
+      </div>
     </div>
-    {isActive && <div className="accordion-content">{content}</div>}
-  </div>
-</div>
   );
 };
 export default Accordion;
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import './style.css';
 import Accordion from './Accordion';
-import { Data } from './utils/data';
 
 function App() {
-  return (
-    <div>
-      <h1>All Open Accordion</h1>
-      <div className='Accordion'>
-        {Data.map(({ title, content }) => (
-            <Accordion title={title} content={content} />
-          ))}
-      </div>
-    </div>
-  );
+  return <Accordion/>;
 }
 
 export default App;
